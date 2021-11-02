8e735ccdfab11fb3cb687789834034d32a447b10
Aravindhan V <aravindhan.v@perfios.com>
2021-11-02 07:45:08 +0530

    ADDED: Handlers to display messaghe with different HTTP requests

--- src/main/java/com/bridgelabz/messagingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/messagingapp/controller/GreetingController.java
@@ -3,11 +3,16 @@ package com.bridgelabz.messagingapp.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.messagingapp.model.Greeting;
+import com.bridgelabz.messagingapp.model.User;
 
 @RestController
 @RequestMapping("/greeting")
@@ -20,4 +25,24 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+	
+	@GetMapping("/query")
+    public Greeting greetingQuery(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/param/{name}")
+    public Greeting greetingParam(@PathVariable String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+	@PostMapping("/post")
+    public Greeting greeting(@RequestBody User user) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, user.getFirstName() + " " + user.getLastName()));
+    }
+
+	@PutMapping("/put/{firstName}")
+    public Greeting greeting(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, firstName + " " + lastName));
+    }
 }
