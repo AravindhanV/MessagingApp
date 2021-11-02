b7c1b9bb66df913a489ddbe90851ae213be5eb48
Aravindhan V <aravindhan.v@perfios.com>
2021-11-02 07:47:30 +0530

    ADDED: GreetingService interface

---
+++ src/main/java/com/bridgelabz/messagingapp/service/IGreetingService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.messagingapp.service;
+
+import com.bridgelabz.messagingapp.model.Greeting;
+import com.bridgelabz.messagingapp.model.User;
+
+public interface IGreetingService {
+
+	Greeting addGreeting(User user);
+
+}
